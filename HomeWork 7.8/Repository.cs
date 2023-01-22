6e2c00d107892835bf0932aa73d08bd5a99de5ed
Sailau Nurbek <112234136+SailauNurbek@users.noreply.github.com>
2023-01-22 11:09:20 +0500

    Add files via upload

---
+++ HomeWork 7.8/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._8
+{
+    internal class Repository
+    {
+        private string fileName = "Worker";
+        public Worker[] GetAllWorkers()
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                // здесь происходит чтение из файла
+                // и возврат массива считанных экземпляров
+
+                string read = sr.ReadToEnd();//Чтение и запись всех данных                           
+                string[] data = read.Split("#");
+
+                Worker[] workers= new Worker[data.Length];
+
+            }
+        }
+
+        #region GetWorkerById
+        /*public Worker GetWorkerById(int id)
+        {
+            // происходит чтение из файла, возвращается Worker
+            // с запрашиваемым ID    
+        }*/
+        #endregion
+
+        public void DeleteWorker(int id)
+        {
+            // считывается файл, находится нужный Worker
+            // происходит запись в файл всех Worker,
+            // кроме удаляемого
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            // присваиваем worker уникальный ID,
+            // дописываем нового worker в файл
+        }
+
+        public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        {
+            // здесь происходит чтение из файла
+            // фильтрация нужных записей
+            // и возврат массива считанных экземпляров
+            return Array.Empty<Worker>();
+        }
+    }
+}
---
+++ HomeWork 7.8/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._8
+{
+    internal struct Worker
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+    }
+}
